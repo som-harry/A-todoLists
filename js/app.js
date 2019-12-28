7eb883def5bd6f760d585584c5f8857930165f3a
Som Harry <chisomekeh71@gmial.com>
2019-12-28 15:43:01 +0100

    fourth commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const clear = document.querySelector(.clear);
+const dataElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+// classes name
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "linethrough";
+
+// shows todays day
+const options = {weekday: "long",month:"short",day:"numeric"};
+const today =  new Date();
+dataElement.innerHTML = today.toLocaleDateString("en-US",options);
